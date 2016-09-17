111174790daf6521f9cc79b7b14a60434524f200
James Wilcox <jrw12@cs.washington.edu>
2016-09-17 15:58:16 -0700

    add insertion sort example

--- Common.v
+++ Common.v
@@ -1,3 +1,5 @@
 Global Set Implicit Arguments.
 Require Export Arith Omega List.
 Export ListNotations.
+
+Require Export StructTactics.
--- Induction.v
+++ Induction.v
@@ -460,3 +460,276 @@ Proof.
   simpl.
   reflexivity.
 Qed.
+
+
+
+(* Insertion sort *)
+
+(* TODO: Discussion of spec for sorting. Usually if you ask people who haven't
+   thought about it before, they will say "well it should be sorted" and not
+   remember to say permutation. *)
+
+Fixpoint insert (a : nat) (xs : list nat) : list nat :=
+  match xs with
+  | [] => [a]
+  | x :: xs => if a <=? x then a :: x :: xs
+              else x :: (insert a xs)
+  end.
+
+(* Just for fun... *)
+Fixpoint insert_tail' (a : nat) (xs acc : list nat) : list nat :=
+  match xs with
+  | [] => List.rev' (a :: acc)
+  | x :: xs => if a <=? x then List.rev_append acc (a :: x :: xs)
+              else insert_tail' a xs (x :: acc)
+  end.
+
+Definition insert_tail (a : nat) (xs : list nat) : list nat :=
+  insert_tail' a xs [].
+
+(* :D *)
+Lemma insert_tail'_is_prepend_reversed_acc_to_insert_xs :
+  forall a xs acc,
+    insert_tail' a xs acc = List.rev acc ++ insert a xs.
+Proof.
+  induction xs.
+  - intros.
+    simpl.
+    unfold rev'.
+    simpl.
+    rewrite rev_append_rev.
+    simpl.
+    reflexivity.
+  - intros.
+    simpl.
+    break_if.
+    + rewrite rev_append_rev.
+      reflexivity.
+    + rewrite IHxs.
+      simpl.
+      rewrite app_ass.
+      simpl.
+      reflexivity.
+Qed.
+
+Theorem insert_tail_is_insert :
+  forall a xs,
+    insert_tail a xs = insert a xs.
+Proof.
+  intros.
+  unfold insert_tail.
+  rewrite insert_tail'_is_prepend_reversed_acc_to_insert_xs.
+  simpl.
+  reflexivity.
+Qed.
+
+Lemma insert_insert_comm :
+  forall a b xs,
+    insert a (insert b xs) = insert b (insert a xs).
+Proof.
+  induction xs.
+  - simpl.
+    repeat break_if.
+    + rewrite Nat.leb_le in *.
+      assert (a = b) by omega.
+      subst.
+      reflexivity.
+    + reflexivity.
+    + reflexivity.
+    + rewrite Nat.leb_nle in *.
+      omega.
+  - simpl.
+    repeat (break_if; simpl).
+    all: try rewrite Nat.leb_le in *.
+    all: try rewrite Nat.leb_nle in *.
+    all: auto.
+    all: try omega.
+    all: try discriminate.
+    + assert (a = b) by omega.
+      subst.
+      reflexivity.
+    + rewrite IHxs.
+      reflexivity.
+Qed.
+
+Fixpoint insertion_sort (xs : list nat) : list nat :=
+  match xs with
+  | [] => []
+  | x :: xs => insert x (insertion_sort xs)
+  end.
+
+Fixpoint insertion_sort_tail' (xs acc : list nat) : list nat :=
+  match xs with
+  | [] => acc
+  | x :: xs => insertion_sort_tail' xs (insert_tail x acc)
+  end.
+
+Definition insertion_sort_tail (xs : list nat) : list nat :=
+  insertion_sort_tail' xs [].
+
+Theorem insertion_sort_tail_is_insertion_sort :
+  forall xs,
+    insertion_sort_tail xs = insertion_sort xs.
+Proof.
+  intros.
+  unfold insertion_sort_tail.
+  induction xs.
+  - simpl.
+    reflexivity.
+  - simpl.
+    rewrite <- IHxs.
+    rewrite insert_tail_is_insert.
+
+    Lemma insertion_sort_tail'_insert_comm :
+      forall xs a ys,
+        insertion_sort_tail' xs (insert a ys) = insert a (insertion_sort_tail' xs ys).
+    Proof.
+      induction xs.
+      - intros.
+        simpl.
+        reflexivity.
+      - intros.
+        simpl.
+        rewrite insert_tail_is_insert.
+        rewrite insert_tail_is_insert.
+        rewrite insert_insert_comm.
+        rewrite IHxs.
+        reflexivity.
+    Qed.
+
+    rewrite insertion_sort_tail'_insert_comm.
+    reflexivity.
+Qed.
+
+(* Roll our own sorted because it's a good exercise and the stdlib is more
+   general than we need. *)
+Inductive sorted : list nat -> Prop :=
+| sorted_nil : sorted []
+| sorted_cons : forall a xs, sorted xs -> List.Forall (fun x => a <= x) xs -> sorted (a :: xs).
+
+Theorem insertion_sort_sorted :
+  forall xs, sorted (insertion_sort xs).
+Proof.
+  induction xs.
+  - simpl.
+    apply sorted_nil.
+  - simpl.
+
+    Lemma insert_sorted :
+      forall a xs,
+        sorted xs ->
+        sorted (insert a xs).
+    Proof.
+      induction xs.
+      - intros.
+        simpl.
+        apply sorted_cons.
+        + apply sorted_nil.
+        + apply Forall_nil.
+      - intros.
+        simpl.
+        break_if.
+        + rewrite Nat.leb_le in *.
+          apply sorted_cons.
+          * assumption.
+          * invc H.
+            apply Forall_cons.
+            -- assumption.
+            -- eapply Forall_impl; try eassumption.
+               intros.
+               simpl in *.
+               omega.
+        + rewrite Nat.leb_nle in *.
+          invc H.
+          apply sorted_cons.
+          * apply IHxs. assumption.
+          *
+            Lemma insert_Forall_le:
+              forall (a b : nat) (xs : list nat),
+                a <= b ->
+                Forall (fun x : nat => a <= x) xs -> Forall (fun x : nat => a <= x) (insert b xs).
+            Proof.
+              intros.
+              (* Pro tip: induct on the Forall instead of the xs. In general,
+                 induct on the "most dependent" thing you have. *)
+              induction H0.
+              - simpl.
+                apply Forall_cons.
+                + assumption.
+                + apply Forall_nil.
+              - simpl.
+                break_if.
+                + apply Forall_cons.
+                  * assumption.
+                  * apply Forall_cons.
+                    -- assumption.
+                    -- assumption.
+                + apply Forall_cons.
+                  * assumption.
+                  * assumption.
+            Qed.
+
+            apply insert_Forall_le.
+            -- omega.
+            -- assumption.
+    Qed.
+
+    apply insert_sorted.
+    assumption.
+Qed.
+
+(* The permutation library is pretty good already, so we can just use it directly. *)
+
+Require Import Permutation.
+
+Theorem insertion_sort_perm :
+  forall xs, Permutation (insertion_sort xs) xs.
+Proof.
+  induction xs.
+  - simpl.
+    reflexivity.
+  - simpl.
+
+    Lemma insert_perm :
+      forall a xs, Permutation (insert a xs) (a :: xs).
+    Proof.
+      induction xs.
+      - simpl.
+        reflexivity.
+      - simpl.
+        break_if.
+        + reflexivity.
+        + (* Pro tip: rewriting with Permutations works and is typically more
+             convenient than trying to apply lemmas directly. *)
+          rewrite perm_swap.
+          apply Permutation_cons.
+          * reflexivity.
+          * apply IHxs.
+    Qed.
+
+    rewrite insert_perm.
+    apply Permutation_cons.
+    + reflexivity.
+    + apply IHxs.
+Qed.
+
+(* We have proved that insertion_sort is correct (sorted and permutation) and
+   that insertion_sort_tail (which is the code we actually want to run) is
+   equivalent to it. We can combine these to get correctness of
+   insertion_sort_tail. *)
+
+Theorem insertion_sort_tail_sorted :
+  forall xs, sorted (insertion_sort_tail xs).
+Proof.
+  intros.
+  rewrite insertion_sort_tail_is_insertion_sort.
+  apply insertion_sort_sorted.
+Qed.
+
+Theorem insertion_sort_tail_perm :
+  forall xs, Permutation (insertion_sort_tail xs) xs.
+Proof.
+  intros.
+  rewrite insertion_sort_tail_is_insertion_sort.
+  apply insertion_sort_perm.
+Qed.
